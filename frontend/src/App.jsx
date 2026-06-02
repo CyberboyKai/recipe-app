cbf19d5f36e6704e7f82373405c3e57e40e03eca
Aleya Banthavong <aleyatb@gmail.com>
2026-06-02 10:36:09 -0400

    feat: recipes page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home.jsx';
+import Recipes from './pages/Recipes.jsx';
 import NotFound from './pages/NotFound.jsx';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/recipes" element={<Recipes />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
