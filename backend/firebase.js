c0c2ba8bd34aa0b4a0e22236b8342bee503052fa
KAI LEE <kailee@Mac.lan>
2026-06-02 09:14:48 -0400

    chore: set up recipe app scaffold

---
+++ backend/firebase.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDtpLXMdWlKwraJM9gRFkEFcOv52mVOGN8',
+  authDomain: 'recipe-app-week3.firebaseapp.com',
+  projectId: 'recipe-app-week3',
+  storageBucket: 'recipe-app-week3.firebasestorage.app',
+  messagingSenderId: '293677228457',
+  appId: '1:293677228457:web:86d3fd9a77fe116e4e64b8',
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return (
+    <h1>RECIPE-APP</h1>
+    // todo setup react router
+  )
+}
+
+export default App;
