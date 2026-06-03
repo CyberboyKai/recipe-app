00ca2c5c755ad19ead436e021b19d984a7630e59
Aleya Banthavong <aleyatb@gmail.com>
2026-06-03 10:27:06 -0400

    feat: add routing for other pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
 
+import Navbar from './components/Navbar.jsx';
+import AdminPage from './pages/AdminPage.jsx';
+import ChatPage from './pages/ChatPage.jsx';
 import Home from './pages/Home.jsx';
-import Recipes from './pages/Recipes.jsx';
+import Login from './pages/Login.jsx';
 import NotFound from './pages/NotFound.jsx';
-
-import Navbar from "./components/Navbar.jsx";
+import Recipes from './pages/Recipes.jsx';
+import SignUp from './pages/SignUp.jsx';
 
 const App = () => {
   return (
     <div className="min-h-screen bg-white">
-      {/* GLOBAL NAVBAR */}
       <Navbar />
 
-      {/* PAGE CONTENT */}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/recipes" element={<Recipes />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
