56ded18d9439c7acf99fda77b6d7f38579ad7b7c
KAI LEE <kailee@Mac.lan>
2026-06-04 01:31:34 -0400

    chore: move firebase config to env

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,22 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey:
-    import.meta.env.VITE_FIREBASE_API_KEY ||
-    'AIzaSyDtpLXMdWlKwraJM9gRFkEFcOv52mVOGN8',
-  authDomain:
-    import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ||
-    'recipe-app-week3.firebaseapp.com',
-  projectId:
-    import.meta.env.VITE_FIREBASE_PROJECT_ID || 'recipe-app-week3',
-  storageBucket:
-    import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ||
-    'recipe-app-week3.firebasestorage.app',
-  messagingSenderId:
-    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '293677228457',
-  appId:
-    import.meta.env.VITE_FIREBASE_APP_ID ||
-    '1:293677228457:web:86d3fd9a77fe116e4e64b8',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
