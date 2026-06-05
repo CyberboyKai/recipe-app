3a515616648612bf65aa5709b9d3f5f00c2ac555
Anushka1010 <136544048+Anushka1010@users.noreply.github.com>
2026-06-05 11:45:13 -0400

    update imports

--- frontend/src/components/ReviewSection.jsx
+++ frontend/src/components/ReviewSection.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "../pages/RecipeDetail.css";
 
 export default function ReviewsSection({ recipeId, currentUser }) {
